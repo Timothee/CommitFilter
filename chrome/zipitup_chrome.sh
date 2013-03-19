465874c94cce40672895d730430058c8411f1674
Timothée Boucher <timothee@gmail.com>
2013-03-18 22:56:09 -0700

    Update zip script to include new options files
    increase version number

--- chrome/zipitup_chrome.sh
+++ chrome/zipitup_chrome.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 zip -r CommitFilter.zip background.js images/*.png manifest.json lib/* \
-	content_script.js commitfilter.css options.html
+	content_script.js commitfilter.css options.*
